3488450e3fc045b766c1d16f2542eefecd6db48f
leena <leenakhote23@gmail.com>
2015-10-06 12:50:37 +0530

    template changes

--- config/publisher_config.py
+++ config/publisher_config.py
@@ -3,8 +3,8 @@ __author__ = 'zeba'
 
 def get_msg(sender, recipient):
     author = "space_provider"
-    category = "space_progress"
-    subject = "space_progress"
+    category = "book_new"
+    subject = "book_new"
 
     msg = {"sender": sender, "recipient": recipient, "author": author, "category": category,
             "subject": subject, "email_content": get_template_content(subject)
@@ -16,5 +16,10 @@ def get_msg(sender, recipient):
 def get_template_content(subject):
        return { "Status of Booking ID: Accepted" : {"name" : "leena" ,"space_id" :"12345" ,"book_id" : "34"} ,
                 "enquiry_reply" :{"name" : "hello" , "date" : "12345678"},
-                "space_progress" : {"name" : "Zeba Dhongre"}
+                "space_progress" : {"name" : "Zeba Dhongre"},
+                "forgot password" : {"name" : "MCO" , "link" : "www.google.com"},
+                "book_new" : {"name" : "lee" ,"space_id" :"675" ,"user_name" : "leena_khote" , "user_industry" : "mco" ,
+                              "space_type" : 'cabin' ,"from_date" :"10/10/2015" , "to_date" :"10/10/2016" , "from_time" : "10 am" ,
+                              "to_time" : "8 pm" , "facility" : "all"} ,
+
        }[subject]
